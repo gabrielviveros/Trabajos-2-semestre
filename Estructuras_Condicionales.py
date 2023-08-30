9fd08b2e7f377413fdd8b13254db0cbf62e279f7
gabrielviveros <gabrielviveros1984@gmail.com>
2023-08-29 22:32:29 -0300

    https://github.com/gabrielviveros/Pro

---
+++ Estructuras_Condicionales.py
@@ -0,0 +1,7 @@
+  #Estructuras Condicionales
+  print ("Bienveiido!")
+  numero = input("Por favor! Ingrese un numero a continuacion: ")
+    no 
+    if numero == "10":
+      print("¡Usted ha ganado el sorteo!")
+    
