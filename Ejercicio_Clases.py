f90b72a37766dd8a889902ad9a926cf4ada40a3a
gabrielviveros <97645126+gabrielviveros@users.noreply.github.com>
2023-09-06 00:25:00 -0300

    Add files via upload

---
+++ Ejercicio_Clases.py
@@ -0,0 +1,15 @@
+class Escuela():
+    nombreEscuela:""
+    direccion:""
+    telefono:0
+    cuerpoDirectivo:""
+    especialidad:""
+    
+    def __init__(self, NombreEscuela, Direccion, Telefono, CuerpoDirectivo, Especialidad):
+        self.nombreEscuela=NombreEscuela
+        self.direccion=Direccion
+        self.telefono=Telefono
+        self.cuerpoDirectivo=CuerpoDirectivo
+        self.especialidad=Especialidad
+        
+        
